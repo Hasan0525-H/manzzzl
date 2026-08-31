2e8bd4ce58c4d485889814e652f3fe9f5e5f9a32
Malik05255 <mmbbrr99@gmail.com>
2026-08-31 18:12:36 +0300

    feat: launch streamlined Manzl hybrid experience

--- manzl-app/src/main/kotlin/com/manzl/app/MainActivity.kt
+++ manzl-app/src/main/kotlin/com/manzl/app/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
-import com.manzl.app.ui.ManzlRoot
+import com.manzl.app.ui.ManzlExperience
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            ManzlRoot()
+            ManzlExperience()
         }
     }
 }
