4e5f3a32b1a1a163fc9009ffc3929a6f0ce563c0
Hasan0525-H <m-b-r9@hotmail.com>
2026-09-02 01:12:21 +0300

    Add home project creation screen structure

---
+++ apps/android/app/src/main/java/com/manzzzl/ai/screens/CreateHomeProjectScreen.kt
@@ -0,0 +1,9 @@
+package com.manzzzl.ai.screens
+
+/**
+ * Initial project creation flow.
+ */
+class CreateHomeProjectScreen {
+    val supportedFloors = listOf("دور واحد", "دورين")
+    val supportedCities = listOf("جدة", "أبها", "محايل عسير", "جازان")
+}
