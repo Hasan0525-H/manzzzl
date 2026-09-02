cf76d5e5658e09bc8a8d6d9df68767c097f36b84
Hasan0525-H <m-b-r9@hotmail.com>
2026-09-02 12:49:37 +0300

    Prepare main flow structure

--- apps/android/app/src/main/java/com/manzzzl/ai/MainActivity.kt
+++ apps/android/app/src/main/java/com/manzzzl/ai/MainActivity.kt
@@ -3,15 +3,26 @@ package com.manzzzl.ai
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import com.manzzzl.ai.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen(
-                onCreateProject = {}
-            )
+            ManzzzlApp()
         }
+    }
+}
+
+@Composable
+fun ManzzzlApp() {
+    MaterialTheme {
+        HomeScreen(
+            onCreateProject = {
+                // Project setup flow will be connected here
+            }
+        )
     }
 }
